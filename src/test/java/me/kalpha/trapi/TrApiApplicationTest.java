1816056c2bce9d40d5aee46f5f6aa59aa8b506de
kalphageek <kalphageek@outlook.com>
2021-05-05 07:46:07 +0900

    hikari 설정 적용

---
+++ src/test/java/me/kalpha/trapi/TrApiApplicationTest.java
@@ -0,0 +1,24 @@
+package me.kalpha.trapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class TrApiApplicationTest {
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    public void contextLoads() throws Exception{
+        System.out.println("============================================================");
+        System.out.println(">>>>>>> Hikari Datasource: {}" + dataSource.getConnection().getMetaData());
+        System.out.println("============================================================");
+    }
+}
